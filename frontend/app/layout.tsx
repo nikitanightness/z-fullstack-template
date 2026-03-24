baa2dd935410755700c880a6e917299061331fcf
Nikita Zagoruyko <nik.nightness@gmail.com>
2026-03-24 20:06:56 +0300

    Some refactoring for layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,28 +1,38 @@
-import { Geist, Geist_Mono, Space_Grotesk, Oxanium } from "next/font/google"
-
-import "./globals.css"
+import { ReactNode } from "react"
+import { JetBrains_Mono, Space_Grotesk, Oxanium } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
+import "./globals.css"
 
-const oxaniumHeading = Oxanium({subsets:['latin'],variable:'--font-heading'});
-
-const spaceGrotesk = Space_Grotesk({subsets:['latin'],variable:'--font-sans'})
-
-const fontMono = Geist_Mono({
+const FONT_OXANIUM = Oxanium({
   subsets: ["latin"],
-  variable: "--font-mono",
+  variable: "--font-oxanium",
+})
+
+const FONT_SPACE_GROTESK = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-space-grotesk",
+})
+
+const FONT_JETBRAINS_MONO = JetBrains_Mono({
+  subsets: ["latin", "cyrillic"],
+  variable: "--font-jetbrains-mono",
 })
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: ReactNode
 }>) {
   return (
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", spaceGrotesk.variable, oxaniumHeading.variable)}
+      className={cn(
+        FONT_SPACE_GROTESK.variable,
+        FONT_JETBRAINS_MONO.variable,
+        FONT_OXANIUM.variable
+      )}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
